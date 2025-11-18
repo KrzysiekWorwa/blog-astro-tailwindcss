bdd734bf1f6ab3e33754ff0731c697fc536133ab
Krzysztof Worwa <worwak1998@gmail.com>
2025-11-18 16:07:23 +0100

    add githubpages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: `https://krzysiekworwa.github.io/blog-astro-tailwindcss/`,
+    base: `/blog-astro-tailwindcss/`
+});
