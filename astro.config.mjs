35e313cdf9f07acb9a0ecc520d463e447fa584d8
Krzysztof Worwa <worwak1998@gmail.com>
2025-11-20 12:43:16 +0100

    add astro islands and dark theme

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from '@astrojs/preact';
+
 // https://astro.build/config
 export default defineConfig({
-    site: `https://krzysiekworwa.github.io/blog-astro-tailwindcss/`,
-    base: `/blog-astro-tailwindcss/`
+  site: `https://krzysiekworwa.github.io/blog-astro-tailwindcss/`,
+  base: `/blog-astro-tailwindcss/`,
+  integrations: [preact()]
 });
